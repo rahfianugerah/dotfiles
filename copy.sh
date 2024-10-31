420e5cef1141a240c221d4d97a82660b1bd4ad1a
Naufal Rahfi Anugerah <156213717+rahfianugerah@users.noreply.github.com>
2024-10-31 15:44:20 +0700

    feat: create copy.sh
    Create script for copying files

---
+++ copy.sh
@@ -0,0 +1,5 @@
+# Copy .zshrc and .bashrc to the home directory
+cp "dotfiles/.zshrc" "$HOME/.zshrc"
+cp "dotfiles/.bashrc" "$HOME/.bashrc"
+# Notify that the files have been copied
+echo ".zshrc and .bashrc have been copied to the home directory."
