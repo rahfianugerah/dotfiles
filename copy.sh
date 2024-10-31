72ff4aa49dcbb719947e0e16475c9a4e0f1aef7b
Naufal Rahfi Anugerah <156213717+rahfianugerah@users.noreply.github.com>
2024-10-31 15:52:23 +0700

    chore: update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,5 +1,9 @@
 # Copy .zshrc and .bashrc to the home directory
 cp "dotfiles/.zshrc" "$HOME/.zshrc"
 cp "dotfiles/.bashrc" "$HOME/.bashrc"
+
 # Notify that the files have been copied
 echo ".zshrc and .bashrc have been copied to the home directory."
+
+# Refresh the shell
+exec "$SHELL"
