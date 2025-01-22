bfeb585858db56e6626df134ffb8888bbeab08bd
rahfianugerah <nrahfia@gmail.com>
2025-01-22 16:56:55 +0700

    feat: create script for installing and checking google chrome, nvm, and vscode

--- arch/run.sh
+++ arch/run.sh
@@ -3,29 +3,51 @@
 cat << "EOF"
         .--.
        |o_o |    config-script: run.sh
-       |:_/ |    version: 1.0 (arch-linux)
+       |:_/ |    version: 1.1 (arch-linux)
       //   \ \   author(github): rahfianugerah
      (|     | )  repository: rahfianugerah/dotfiles
     /'\_   _/'\
     \___)=(___/
 EOF
 
-# Prompt for user name for Git configuration
-read -p "Enter your name for Git configuration: " name
-# Prompt for user email for Git configuration
-read -p "Enter your email for Git configuration: " email
-# Prompt for the desired default branch name
-read -p "Enter the default branch name: " branch_name
+# Install Google Chrome
+if ! pacman -Qi google-chrome &>/dev/null; then
+  echo "Installing Google Chrome..."
+  echo "Adding repository for Google Chrome..."
+  echo -e "\n[custom-google-chrome]\nServer = https://aur.archlinux.org/packages" | sudo tee -a /etc/pacman.conf
+  sudo pacman -Sy
+  git clone https://aur.archlinux.org/google-chrome.git
+  cd google-chrome
+  makepkg -si --noconfirm
+  cd ..
+  rm -rf google-chrome
+else
+  echo "Google Chrome is already installed."
+fi
 
-# Set the global Git user name
-git config --global user.name "$name"
-# Set the global Git user email
-git config --global user.email "$email"
-# Set the default branch name
-git config --global init.defaultBranch "$branch_name"
+# Install NVM
+if [ ! -d "$HOME/.nvm" ]; then
+  echo "Installing NVM..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+  echo "export NVM_DIR=\"$HOME/.nvm\"" >> ~/.bashrc
+  echo "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\" # This loads nvm" >> ~/.bashrc
+  echo "[ -s \"$NVM_DIR/bash_completion\" ] && . \"$NVM_DIR/bash_completion\" # This loads nvm bash_completion" >> ~/.bashrc
+  source ~/.bashrc
+else
+  echo "NVM is already installed."
+fi
 
-# Confirmation message
-echo "Git configuration was successful!"
-echo "User name: $name"
-echo "User email: $email"
-echo "Default branch name: $branch_name"
+# Install Visual Studio Code
+if ! pacman -Qi code &>/dev/null; then
+  echo "Installing Visual Studio Code..."
+  git clone https://aur.archlinux.org/visual-studio-code-bin.git
+  cd visual-studio-code-bin
+  makepkg -si --noconfirm
+  cd ..
+  rm -rf visual-studio-code-bin
+else
+  echo "Visual Studio Code is already installed."
+fi
+
+# Final message
+echo "All requested software has been installed or is already present."
