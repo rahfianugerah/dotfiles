3856ef0b847b3eafd110411f7654022d9895088c
rahfianugerah <nrahfia@gmail.com>
2025-01-22 09:48:20 +0700

    feat: updated bash code for copying the rc files

--- deb/copy.sh
+++ deb/copy.sh
@@ -1,6 +1,6 @@
 # Copy .zshrc and .bashrc to the home directory
-cp "dotfiles/.zshrc" "$HOME/.zshrc"
-cp "dotfiles/.bashrc" "$HOME/.bashrc"
+cp "dotfiles/deb/.zshrc" "$HOME/.zshrc"
+cp "dotfiles/deb/.bashrc" "$HOME/.bashrc"
 
 # Notify that the files have been copied
 echo ".zshrc and .bashrc have been copied to the home directory."
