9df676b82224919810589e51dfbfdf7e5083c21a
rahfianugerah <nrahfia@gmail.com>
2026-03-13 12:56:41 +0700

    feat: update the the copy.sh script

--- wsl/copy.sh
+++ wsl/copy.sh
@@ -1,9 +1,8 @@
 # Copy .zshrc and .bashrc to the home directory
-cp "dotfiles/wsl/.zshrc" "$HOME/.zshrc"
 cp "dotfiles/wsl/.bashrc" "$HOME/.bashrc"
 
 # Notify that the files have been copied
-echo ".zshrc and .bashrc have been copied to the home directory."
+echo ".bashrc have been copied to the home directory."
 
 # Refresh the shell
 exec "$SHELL"
